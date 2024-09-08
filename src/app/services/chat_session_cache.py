8848c4d3cb237afaec575546aa08efcf761d7f9e
Udit <23015406+udit-001@users.noreply.github.com>
2024-09-09 01:45:35 +0530

    fix: remove else condition

--- src/app/services/chat_session_cache.py
+++ src/app/services/chat_session_cache.py
@@ -9,16 +9,12 @@ class ChatSessionCache:
     def add_session(self, chatbot: Chatbot, user_id: str):
         self.cache[f"{user_id}:{chatbot.persona.name}"] = chatbot
 
-    def session_exists(self, user_id: str):
-        return True if user_id in self.cache else False
-
     def get_or_create_session(self, user_id: str, persona: str):
         if f"{user_id}:{persona}" in self.cache.keys():
             return self.cache[f"{user_id}:{persona}"]
-        else:
-            chatbot = Chatbot(persona=personas[persona], user_id=user_id)
-            self.add_session(chatbot, user_id)
-            return chatbot
+        chatbot = Chatbot(persona=personas[persona], user_id=user_id)
+        self.add_session(chatbot, user_id)
+        return chatbot
 
 
 CHATBOT_CACHE = ChatSessionCache()
